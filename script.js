fae66838a40e03b2a7b1389e0c6579413ee4d949
RannoLauri <rannolauri@gmail.com>
2020-05-07 14:41:33 +0300

    fixed size sketchboard. Buttons to clear and choose pixel count on the sketchboard

--- script.js
+++ script.js
@@ -3,11 +3,26 @@ const container = document.querySelector('#container');
 function makeGrid(cols, rows){
     container.style.setProperty('--grid-cols', cols);
     container.style.setProperty('--grid-rows', rows);
+
     for(i= 0; i< (cols * rows); i++){
         const cell = document.createElement('div');
-        cell.textContent = i + 1;
-        container.appendChild(cell).className = 'gridElement';
+        cell.addEventListener("mouseenter", e => { 
+            e.target.classList.add("hovered")})
+        container.appendChild(cell).classList.add('gridPixel');
     }
+}
+
+function clearCanvas(){
+    let hovered = document.querySelectorAll('.hovered');
+    for (i= 0; i< hovered.length; i++){
+        hovered[i].classList.remove('hovered')
+    }
+}
+
+function makeCanvas(){
+    let size = prompt("Choose the pixel count.");
+    clearCanvas();
+    makeGrid(size,size);
 }
 
 makeGrid(16,16);
